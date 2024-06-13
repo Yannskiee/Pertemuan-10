8758bce351157879e111d2002c6add208e91109f
Your Name <you@example.com>
2024-06-13 15:00:36 +0700

    yh

---
+++ Challenge2.php
@@ -0,0 +1,29 @@
+<?php 
+$angka1=9;
+$angka2=8;
+
+    
+    $myName = $_POST['Sumo'];
+    $angkaPertama= $_POST['angkaPertama'];
+    $angkaKedua= $_POST['angkaKedua'];
+
+
+    $tambah = $angkaPertama + $angkaKedua;
+    $kurang = $angkaPertama - $angkaKedua; 
+    $perkalian = $angkaPertama * $angkaKedua; 
+    $pembagian = $angkaPertama / $angkaKedua; 
+    $modulus = $angkaPertama % $angkaKedua; 
+
+    echo "<h2> Perhitungan Aritmatika </h2>"; 
+    
+    
+
+    echo "Nama : $myName <br>";
+
+    echo "$angkaPertama + $angkaKedua = $tambah<br>"; 
+    echo "$angkaPertama - $angkaKedua = $kurang<br>"; 
+    echo "$angkaPertama * $angkaKedua = $perkalian<br>"; 
+    echo "$angkaPertama / $angkaKedua = $pembagian<br>"; 
+    echo "$angkaPertama % $angkaKedua = $modulus";
+
+    ?>
